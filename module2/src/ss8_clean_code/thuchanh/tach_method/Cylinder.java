b96a36ccbfaaa9cbcbbf1efa6a9550adb888b767
git config --global user.name <phuong110917@gmail.com>
2021-10-25 19:39:28 +0700

    .

--- module2/src/ss8_clean_code/thuchanh/tach_method/Cylinder.java
+++ module2/src/ss8_clean_code/thuchanh/tach_method/Cylinder.java
@@ -2,9 +2,16 @@ package ss8_clean_code.thuchanh.tach_method;
 
 public class Cylinder {
     public static double getVolume(int radius, int height){
-        double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
         double volume = perimeter * height + 2 * baseArea;
         return volume;
+    }
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI  * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
     }
 }
