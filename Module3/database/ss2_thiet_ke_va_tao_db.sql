c5abbb77934a3a093485ce474ea652a4c69f1b98
git config --global user.name <phuong110917@gmail.com>
2021-12-04 11:39:30 +0700

    .

--- Module3/database/ss2_thiet_ke_va_tao_db.sql
+++ Module3/database/ss2_thiet_ke_va_tao_db.sql
@@ -43,58 +43,64 @@ foreign key(ma_giang_vien) references giang_vien(ma_giang_vien),
 foreign key(ma_lop_hoc) references  lop_hoc(ma_lop_hoc)
 );
 
-insert into `tai_khoan` values("TanHai", "19980101");
-insert into `tai_khoan` values("TranTan", "20010101");
-insert into `tai_khoan` values("VanTu", "20010410");
-insert into `tai_khoan` values("VanMy", "20021109");
-insert into `tai_khoan` values("NgoTri", "19971030");
-insert into `tai_khoan` values("ThanhHai", "19981130");
-insert into `tai_khoan` values("BuiTy", "19970529");
+insert into `tai_khoan`(username, `password`)
+values("TanHai", "19980101"),
+      ("TranTan", "20010101"),
+      ("VanTu", "20010410"),
+	  ("VanMy", "20021109"),
+	  ("NgoTri", "19971030"),
+	  ("ThanhHai", "19981130"),
+      ("BuiTy", "19970529");
 select * from `tai_khoan`;
 
-insert into lop_hoc values("024101", "Toan K24");
-insert into lop_hoc values("024102", "Van K24");
-insert into lop_hoc values("024103", "Ly K24");
-insert into lop_hoc values("024104", "Hoa K24");
-insert into lop_hoc values("024105", "Su K24");
-insert into lop_hoc values("024106", "Dia K24");
-insert into lop_hoc values("024107", "Tieng Anh K24");
+insert into lop_hoc (ma_lop_hoc, ten_lop_hoc)
+values("024101", "Toan K24"),
+      ("024102", "Van K24"),
+      ("024103", "Ly K24"),
+      ("024104", "Hoa K24"),
+      ("024105", "Su K24"),
+      ("024106", "Dia K24"),
+      ("024107", "Tieng Anh K24");
 select * from lop_hoc;
 
-insert into giang_vien values("01020001", "Nguyen Thi Ngoc Anh", "1983-03-21");
-insert into giang_vien values("01020002", "Nguyen Cong Binh", "1979-06-06");
-insert into giang_vien values("01020003", "Ngo Viet Bac", "1982-03-11");
-insert into giang_vien values("01020004", "Tran Thi Kim Anh", "1982-11-04");
-insert into giang_vien values("01020005", "Nguyen Tuan Anh", "1979-07-15");
-insert into giang_vien values("01020006", "Ho Dang Chien", "1982-01-20");
-insert into giang_vien values("01020007", "Ngo Thi Nhat Anh", "1982-11-27");
+insert into giang_vien (ma_giang_vien, ten_giang_vien, ngay_sinh)
+values("01020001", "Nguyen Thi Ngoc Anh", "1983-03-21"),
+	  ("01020002", "Nguyen Cong Binh", "1979-06-06"),
+      ("01020003", "Ngo Viet Bac", "1982-03-11"),
+      ("01020004", "Tran Thi Kim Anh", "1982-11-04"),
+      ("01020005", "Nguyen Tuan Anh", "1979-07-15"),
+      ("01020006", "Ho Dang Chien", "1982-01-20"),
+      ("01020007", "Ngo Thi Nhat Anh", "1982-11-27");
 select * from giang_vien;
 
-insert into hoc_vien values("01241020001", "Truong Tan Hai", "1998-01-01", "Hai98@gmail.com", "TanHai", "024101");
-insert into hoc_vien values("01241020002", "Tran Tan Tai", "2001-01-01", "Tai01@gmail.com", "TranTan", "024102");
-insert into hoc_vien values("01241020003", "Nguyen Van Tu", "2001-04-10", "Tu010410@gmail.com", "VanTu", "024103");
-insert into hoc_vien values("01241020004", "Nguyen Van My", "2002-11-09", "Vanmy02@gmail.com", "VanMy", "024104");
-insert into hoc_vien values("01241020005", "Ngo Tri Tai", "1997-10-30", "Tritai97@gmail.com", "NgoTri", "024105");
-insert into hoc_vien values("01241020006", "Nguyen Thanh Hai", "1998-11-30", "Thanhhai98@gmail.com", "ThanhHai", "024106");
-insert into hoc_vien values("01241020007", "Mai Bui Ty", "1997-05-29", "Buity97@gmail.com", "BuiTy", "024107");
+insert into hoc_vien (ma_hoc_vien, ten_hoc_vien, ngay_sinh, email, username, ma_lop_hoc )
+values("01241020001", "Truong Tan Hai", "1998-01-01", "Hai98@gmail.com", "TanHai", "024101"),
+      ("01241020002", "Tran Tan Tai", "2001-01-01", "Tai01@gmail.com", "TranTan", "024102"),
+      ("01241020003", "Nguyen Van Tu", "2001-04-10", "Tu010410@gmail.com", "VanTu", "024103"),
+      ("01241020004", "Nguyen Van My", "2002-11-09", "Vanmy02@gmail.com", "VanMy", "024104"),
+      ("01241020005", "Ngo Tri Tai", "1997-10-30", "Tritai97@gmail.com", "NgoTri", "024105"),
+      ("01241020006", "Nguyen Thanh Hai", "1998-11-30", "Thanhhai98@gmail.com", "ThanhHai", "024106"),
+      ("01241020007", "Mai Bui Ty", "1997-05-29", "Buity97@gmail.com", "BuiTy", "024107");
 select * from hoc_vien;
 
-insert into dia_chi values("020001", "Hoa Khanh", "01241020002");
-insert into dia_chi values("020002", "Nguyen Van Linh", "01241020003");
-insert into dia_chi values("020003", "Thanh Khue", "01241020001");
-insert into dia_chi values("020004", "Lien Chieu", "01241020005");
-insert into dia_chi values("020005", "Hoa Khanh", "01241020007");
-insert into dia_chi values("020006", "Hoa Vang", "01241020004");
-insert into dia_chi values("020007", "Quang Trung", "01241020006");
+insert into dia_chi (ma_dia_chi, dia_chi, ma_hoc_vien)
+values("020001", "Hoa Khanh", "01241020002"),
+      ("020002", "Nguyen Van Linh", "01241020003"),
+      ("020003", "Thanh Khue", "01241020001"),
+      ("020004", "Lien Chieu", "01241020005"),
+      ("020005", "Hoa Khanh", "01241020007"),
+      ("020006", "Hoa Vang", "01241020004"),
+      ("020007", "Quang Trung", "01241020006");
 select * from dia_chi;
 
-insert into giang_vien_day_lop_hoc value ("01020002", "024101");
-insert into giang_vien_day_lop_hoc value ("01020001", "024103");
-insert into giang_vien_day_lop_hoc value ("01020004", "024105");
-insert into giang_vien_day_lop_hoc value ("01020003", "024102");
-insert into giang_vien_day_lop_hoc value ("01020007", "024104");
-insert into giang_vien_day_lop_hoc value ("01020006", "024106");
-insert into giang_vien_day_lop_hoc value ("01020005", "024107");
+insert into giang_vien_day_lop_hoc (ma_giang_vien, ma_lop_hoc)
+value ("01020001", "024107"),
+      ("01020002", "024103"),
+      ("01020003", "024106"),
+      ("01020004", "024102"),
+      ("01020005", "024104"),
+      ("01020006", "024105"),
+      ("01020007", "024101");
 select * from giang_vien_day_lop_hoc
 
 
---
+++ Module3/ss2_database_desgin/baitap/quanlybanhang.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE QuanLyBanHang;
+
+USE QuanLyBanHang;
+
+CREATE TABLE Customer(
+      cID INT PRIMARY KEY,
+      cName VARCHAR(50) NOT NULL,
+      cAge INT NOT NULL
+);
+
+CREATE TABLE `Order`(
+      oID INT PRIMARY KEY,
+	  cID INT NOT NULL,
+      oDate DATE,
+      oTotalPrice DOUBLE,
+      FOREIGN KEY (cID) REFERENCES Customer(cID)
+);
+
+CREATE TABLE Product(
+	  pID INT PRIMARY KEY,
+      pName VARCHAR(50) NOT NULL,
+      pPrice DOUBLE
+);
+
+CREATE TABLE OrderDetail(
+      oID INT NOT NULL,
+      pID INT NOT NULL,
+      odQTY VARCHAR(100) NOT NULL,
+      PRIMARY KEY (oID, pID),
+      FOREIGN KEY (oID) REFERENCES `Order`(oID),
+      FOREIGN KEY (pID) REFERENCES Product(pID)
+);
